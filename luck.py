bea7235cd795221327e8dee8f6430808232c89ae
Simran Biswas <48387438+simranbiswas@users.noreply.github.com>
2021-02-27 12:16:16 +0530

    Create luck.py

---
+++ luck.py
@@ -0,0 +1,13 @@
+contests.sort(reverse=True)
+    luck = 0
+
+    for contest in contests:
+        if contest[1] == 0:
+            luck += contest[0]
+        elif k > 0:
+            luck += contest[0]
+            k -= 1
+        else:
+            luck -= contest[0]
+
+    return luck
